5ff36e7b31314eb12ea6a0d6f6e8b451a5ba2665
Matthew Yee <32374888+MatthewYee92@users.noreply.github.com>
2019-12-12 10:44:09 -0800

    updated formatting around the spacing

--- rescursiveReverseLL.js
+++ rescursiveReverseLL.js
@@ -3,7 +3,7 @@ const LinkedListNode = (value) => {
   this.next = null;
 }
 
-const reverse = head => {
+const reverse = (head) => {
   if(!head || !head.next) return head;
   let tmp = reverse(head.next);
   head.next.next = head;
